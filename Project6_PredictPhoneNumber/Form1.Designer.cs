ebae3a151dd6c80a3cdceb791c090af010b2c583
pksrach <pksrach@gmail.com>
2022-03-07 02:17:57 +0700

    Update project 3

--- Project6_PredictPhoneNumber/Form1.Designer.cs
+++ Project6_PredictPhoneNumber/Form1.Designer.cs
@@ -29,20 +29,142 @@ namespace Project6_PredictPhoneNumber
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnOK = new System.Windows.Forms.Button();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbExit = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.BackColor = System.Drawing.Color.White;
+            this.btnOK.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnOK.BackgroundImage")));
+            this.btnOK.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnOK.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOK.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnOK.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnOK.Location = new System.Drawing.Point(919, 149);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(0);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(161, 47);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnOK.UseVisualStyleBackColor = false;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtPhone.Location = new System.Drawing.Point(383, 158);
+            this.txtPhone.MaxLength = 11;
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(433, 28);
+            this.txtPhone.TabIndex = 1;
+            this.txtPhone.TextChanged += new System.EventHandler(this.txtPhone_TextChanged);
+            this.txtPhone.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPhone_KeyDown);
+            this.txtPhone.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPhone_KeyPress);
+            // 
+            // lbResult
+            // 
+            this.lbResult.BackColor = System.Drawing.Color.Transparent;
+            this.lbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResult.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lbResult.Location = new System.Drawing.Point(322, 270);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(130, 56);
+            this.lbResult.TabIndex = 2;
+            this.lbResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbResult.TextChanged += new System.EventHandler(this.lbResult_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("SN KH WAT CHROY THMOR", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.richTextBox1.Location = new System.Drawing.Point(76, 398);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1036, 221);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("SN KH WAT CHROY THMOR", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.Location = new System.Drawing.Point(1029, 276);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 47);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "សម្អាត";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbExit
+            // 
+            this.lbExit.AutoSize = true;
+            this.lbExit.BackColor = System.Drawing.Color.Transparent;
+            this.lbExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbExit.Font = new System.Drawing.Font("SN KH WAT CHROY THMOR", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbExit.ForeColor = System.Drawing.Color.White;
+            this.lbExit.Location = new System.Drawing.Point(12, 9);
+            this.lbExit.Name = "lbExit";
+            this.lbExit.Size = new System.Drawing.Size(77, 34);
+            this.lbExit.TabIndex = 6;
+            this.lbExit.Text = "ចាកចេញ";
+            this.lbExit.Click += new System.EventHandler(this.lbExit_Click);
+            this.lbExit.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lbExit_MouseDown);
+            this.lbExit.MouseEnter += new System.EventHandler(this.lbExit_MouseEnter);
+            this.lbExit.MouseLeave += new System.EventHandler(this.lbExit_MouseLeave);
+            this.lbExit.MouseHover += new System.EventHandler(this.lbExit_MouseHover);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1175, 596);
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1184, 681);
+            this.Controls.Add(this.lbExit);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.btnOK);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Predict Phone Number";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbExit;
     }
 }
 
--- Project6_PredictPhoneNumber/Form1.cs
+++ Project6_PredictPhoneNumber/Form1.cs
@@ -7,14 +7,151 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace Project6_PredictPhoneNumber
 {
     public partial class Form1 : Form
     {
+        int result = 0, sumJ = 0;
+        Hashtable hash = new Hashtable();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            txtPhone.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hash.Add(1, "អ្នកជាមនុស្សមាន អំណាចចូលចិត្តធ្វើជាមេដឹកនាំគេ ។ ប៉ុន្តែជាមនុស្ស អន្ទះសារអ៊ូរទាំច្រើន ចិត្តរាយមាយឆាប់ខឹងឆាប់បាត់ ។");
+            hash.Add(2, "អ្នកជាមនុស្សឆាប់ ញាប់ញ័រ មានមន្តស្នេហ៍ ព្រោះនិយាយស្តីផ្អែមល្ហែម ចូលចិត្តធ្វើអ្វីស្ងាត់ៗម្នាក់ឯង សម្បកក្រៅមើលទៅជាមនុស្សកាចនិងម៉ឺងម៉ាត់ ប៉ុន្តែ ដឹងអីខាងក្នុងរបស់អ្នកវិញទន់ជ្រាយតែម្តង ។");
+            hash.Add(3, "អ្នកជាមនុស្ស និយាយសំឡេងខ្លាំងៗឮៗជាមនុស្សខ្ជិល ។ ចំណុចល្អ របស់អ្នកគឺជាមនុស្សមានការតាំងចិត្តច្បាស់លាស់ប្រាកដប្រជា បើគិតចង់ធ្វើអ្វីមួយ ហើយ គឺត្រូវធ្វើឱ្យបានសម្រេចមិនទុកចោលកណ្តាលទីឡើយ ។");
+            hash.Add(4, "អ្នកគឺជាមនុស្ស ប្រកបដោយវោហារសព្ទពូកែទ្រឹស្តីនិយាយហើយ អ្នក ស្តាប់ចូលចិត្តមិនមានអារម្មណ៍ធុញទ្រាន់ ។ ម្ល៉ោះហើយបានជាមានគេចូលចិត្ត ច្រើន និងមានមិត្តភក្តិច្រើន ប៉ុន្តែមានចរិតចូលចិត្តផ្លាស់ប្តូរដៃគូស្នេហា ។");
+            hash.Add(5, "អ្នកជាមនុស្សមានហេតុផលគិតមុនធ្វើមានភាពវៃឆ្លាត មិនឆេវឆាវ ប៉ិនអាណិតគេ មិនចូលចិត្តសម្តីកុហក ។");
+            hash.Add(6, "អ្នកជាមនុស្សដែលសម្បូរស្នេហ៍ ច្រើនតែចំណាយប្រាក់ទៅលើការតែង ខ្លួននិងការសប្បាយផ្សេងៗ ។");
+            hash.Add(7, "អ្នកជាមនុស្សឆាប់ ភ័យ ឆាប់តក់ស្លុត ឆាប់ខ្វល់ខ្វាយ ។ ពេលណាខឹងខ្លាំង ច្រើនតែបាត់បង់ម្ចាស់ការ និយាយស្តីច្រើនតែខុស ។ ជួនកាលរឿងមិនត្រូវជា បញ្ហាសោះ ប៉ុន្តែពេលខឹងបែរជានិយាយបង្កើតបញ្ហាធំទៅវិញ ។ ដូច្នេះត្រូវ ព្យាយាមធ្វើចិត្តឱ្យត្រជាក់គិតសិនមុននឹងនិយាយ ។");
+            hash.Add(8, "អ្នកជាមនុស្សឆាប់ជឿគេ ឆាប់ទុកចិត្តគេមាននិស្ស័យជាមនុស្សហ៊ានស៊ី ហ៊ានសង ។");
+            hash.Add(9, "អ្នកជាមនុស្សបុរាណនិយម អភិរក្សនិយមប្រសិនបើអាយុនៅក្មេងក៏មាន ទស្សនៈបែបជាមនុស្សចាស់ដែរ ។");
+        }
+
+        private void txtPhone_TextChanged(object sender, EventArgs e)
+        {
+            if (txtPhone.Text == "")
+            {
+                lbResult.Text = "";
+                richTextBox1.Text = "";
+            }
+        }
+
+        private void lbExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtPhone.Clear();
+            richTextBox1.Clear();
+            lbResult.Text = "";
+            txtPhone.Focus();
+        }
+
+        private void lbExit_MouseHover(object sender, EventArgs e)
+        {
+            //lbExit.ForeColor = Color.Red;
+        }
+
+        private void lbExit_MouseLeave(object sender, EventArgs e)
+        {
+            lbExit.ForeColor = Color.White;
+        }
+
+        private void lbExit_MouseEnter(object sender, EventArgs e)
+        {
+            lbExit.ForeColor = Color.Red;
+        }
+
+        private void lbExit_MouseDown(object sender, MouseEventArgs e)
+        {
+            lbExit.ForeColor = Color.DarkRed;
+        }
+
+        private void txtPhone_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtPhone_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnOK_Click(sender, e);
+            }
+        }
+
+        private void lbResult_TextChanged(object sender, EventArgs e)
+        {
+            if (lbResult.Text == "")
+            {
+                richTextBox1.Clear();
+            }
+            else
+            {
+                int reResult = Convert.ToInt32(lbResult.Text);
+                richTextBox1.Text = hash[reResult].ToString();
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (txtPhone.Text == "")
+            {
+                txtPhone.Focus();
+                richTextBox1.Clear();
+            }
+            else
+            {
+                if (txtPhone.TextLength >= 9)
+                {
+                    string numberPhone = txtPhone.Text;
+                    while (numberPhone.Length > 1)
+                    {
+                        char[] ch = numberPhone.ToCharArray();
+                        int j = 0;
+                        int sum = 0;
+                        for (int i = 0; i < ch.Length; i++)
+                        {
+                            if (sumJ > 2)
+                            {
+                                sum += int.Parse(ch[i].ToString());
+                            }
+                            sumJ += j++;
+                        }
+                        numberPhone = sum.ToString();
+                        result = sum;
+                    }
+                    lbResult.Text = Convert.ToString(result);
+                    sumJ = 0;
+                    result = 0;
+                }
+                else
+                {
+                    
+                    sumJ = 0;
+                    lbResult.Text = "";
+                }
+                if (result > 0)
+                {
+                    richTextBox1.Text = hash[result].ToString();
+                }
+            }
         }
     }
 }
