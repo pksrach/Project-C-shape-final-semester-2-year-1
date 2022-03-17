753f7230c9ff0fd22ffc65c473933c749bc3e86b
SuonPisey <suonpisey017@gmail.com>
2022-03-17 19:13:14 +0700

    get project from somrach

--- Project6_PredictPhoneNumber/frmMaster.Designer.cs
+++ Project6_PredictPhoneNumber/frmMaster.Designer.cs
@@ -123,6 +123,7 @@ namespace Project6_PredictPhoneNumber
             this.Margin = new System.Windows.Forms.Padding(5, 8, 5, 8);
             this.Name = "frmMaster";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.Load += new System.EventHandler(this.frmMaster_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
